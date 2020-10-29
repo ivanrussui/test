ffe16d160ffcc855466b4a7ffceeba72615ed608
Ivan <fenomen_ny@mail.ru>
2020-10-29 17:54:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function createNameBook() {
+  console.log('Notebook');
+}
+
+createNameBook();
+
+let myName;
+
+function openName() {
+  myName = 'Ivan';
+  console.log(myName);
+}
+
+openName();
+
+
+
+let myFrends = ['leonardo', 'donatelo', 'mickelangelo', 'rafael'];
+
+console.log(myFrends.push('shredder'));
+
+console.log(myFrends[4]);
+
+
+
+
